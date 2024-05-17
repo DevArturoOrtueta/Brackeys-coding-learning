c976abc00aefa4a0f34d0ecad0025fb6dbb6ece3
Arturo Ortueta <69780116+DevArturoOrtueta@users.noreply.github.com>
2024-05-16 21:10:32 -0500

    Update

--- Node2D.gd
+++ Node2D.gd
@@ -4,3 +4,12 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("Hello world")
+	$Label.text = 'Hello World'
+	$Label.modulate = Color.GREEN
+
+func _input(event):
+	if event.is_action_pressed("MY_ACTION"):
+		$Label.modulate = Color.RED
+	
+	if event.is_action_released("MY_ACTION"):
+		$Label.modulate = Color.GREEN
