599fe88a164a6038d8301f743a5cdb8ce3e6cb72
Arturo Ortueta <69780116+DevArturoOrtueta@users.noreply.github.com>
2024-05-20 14:41:47 -0500

    Functions

--- Node2D.gd
+++ Node2D.gd
@@ -6,12 +6,24 @@ var health = 100
 # @export var health = 100
 func _ready():
 	print("Hello world")
+	
 	$Label.text = 'Hello World'
 	$Label.modulate = Color.GREEN
 
-func _input(event):
+func _input(event): 
 	if event.is_action_pressed("MY_ACTION"):
-		$Label.modulate = Color.RED
+		var result = add(1,2)
+		result = add(result, 4)
+		print(result)
+	#if event.is_action_pressed("MY_ACTION"):
+	#	$Label.modulate = Color.RED
 	
-	if event.is_action_released("MY_ACTION"):
-		$Label.modulate = Color.GREEN
+	#if event.is_action_released("MY_ACTION"):
+	#	$Label.modulate = Color.GREEN
+
+func jump():
+	print("JUMP!")
+
+func add(num1: int, num2: int) -> int:
+	var result = num1 + num2
+	return result
