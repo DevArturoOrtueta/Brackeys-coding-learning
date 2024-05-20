ef564dd6c5c493d33667730a7385c9d87534bd58
Arturo Ortueta <69780116+DevArturoOrtueta@users.noreply.github.com>
2024-05-20 16:25:57 -0500

    Signals

--- signal.gd
+++ signal.gd
@@ -1,10 +1,15 @@
 extends Node2D
-
-
+signal leveled_up(msg)
+var xp : = 0
 
 func _ready():
-	pass 
+	leveled_up.connect(_on_leveled_up)
+func _on_timer_timeout():
+	xp += 5
+	print(xp)
+	if xp >= 20:
+		xp = 0
+		leveled_up.emit("GZ")
 
-
-func _on_button_pressed():
-	print("MONEY")
+func _on_leveled_up(msg):
+	print(msg)
